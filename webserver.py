e707e0119d48e8e5b2eb9f490ebbd9856a97f247
Sirallens <jorge.lecea01@utrgv.edu>
2019-05-22 15:32:56 -0500

    webserver - update

--- webserver.py
+++ webserver.py
@@ -3,4 +3,8 @@ from waitress import serve
 
 # Arguments: host=(hostname),  port=(port to listen),  listen=(hostname:port to listen)  
 #
-serve(app.app, listen='127.0.0.1:8080')
+localhost = '127.0.0.1:8000'  # locally host the server
+network_host = '0.0.0.0:8080' # host the server using PC's ip inside a network
+
+if __name__ == '__main__':
+  serve(app.app, listen=network_host)
